c630a369abd85005bf3f30e9f4b5a1a866358f85
Victor Sint Nicolaas <vicsn@users.noreply.github.com>
2025-01-20 14:34:28 +0100

    remove incorrect comment

--- synthesizer/process/src/lib.rs
+++ synthesizer/process/src/lib.rs
@@ -239,7 +239,7 @@ impl<N: Network> Process<N> {
         let timer = timer!("Process::load_from_storage");
 
         let storage_mode = storage_mode.ok_or_else(|| anyhow!("Failed to get storage mode"))?;
-        // Try to lazily load the stack.
+
         #[cfg(feature = "rocks")]
         let store = ConsensusStore::<N, ConsensusDB<N>>::open(storage_mode);
         #[cfg(not(feature = "rocks"))]
