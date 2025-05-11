cc91dba7cc277a5b75f3be5ae3c57de134a1edea
Alessandro Coglio <acoglio@provable.com>
2025-05-10 22:05:32 -0700

    [Feature] Review, fix, document, test SHA-2.

--- circuit/algorithms/src/sha2/hash.rs
+++ circuit/algorithms/src/sha2/hash.rs
@@ -19,32 +19,51 @@ impl<E: Environment, const VARIANT: usize> Hash for Sha2<E, VARIANT> {
     type Input = Boolean<E>;
     type Output = Vec<Boolean<E>>;
 
+    /// Apply SHA-224 or SHA-256 to the input bits.
+    /// The bits do not have to be padded; this function does that.
+    // TODO: probably don't inline?
     #[inline]
     fn hash(&self, input: &[Self::Input]) -> Self::Output {
-        if input.is_empty() {
-            return E::halt("The input to the SHA-2 hash function must not be empty");
-        }
-        let mut bits = pad_sha2(input);
-        let chunks = bits.chunks(CHUNK_SIZE);
-        let mut state = self.initial_state.clone();
 
+        // Checking this is likely overkill, but it is the documented limit on message size.
+        // See FIPS PUB 180-4, Figure 1.
+        if input.len() as u128 >= 2u128.pow(64) {
+            // TODO: don't panic and handle the error more gracefully
+            panic!("Input to SHA-224 or SHA-256 must be less than 2^64 bits");
+        }
+        // Note that an empty input is valid and allowed.
+
+        let bits = pad_sha2(input);
+        let chunks = bits.chunks(BLOCK_SIZE);
+
+        let constants: [U32<E>; 64] = std::array::from_fn(|i| U32::constant(console::U32::new(K[i])));
+
+        // This is the current hash, initially `H^(0)`, eventually `H^(N)` (see FIPS PUB 180-4, Section 6.2).
+        let mut state: [U32<E>; 8] = match VARIANT {
+            256 => std::array::from_fn(|i| U32::constant(console::U32::new(H0_256[i]))),
+            224 => std::array::from_fn(|i| U32::constant(console::U32::new(H0_224[i]))),
+            _ => unreachable!("Invalid SHA-2 variant"),
+        };
+
+        // This loop corresponds to the top-level loop 'For i = 1 to N' in FIPS PUB 180-4, Section 6.2.2.
         for chunk in chunks {
-            // Message schedule array with zero initialization using std::array::from_fn.
+
+            // Step 1 in FIPS PUB 180-4 Section 6.2.2.
+            // TODO: should we avoid the circuit constants in the initialization of the array?
             let mut w: [U32<E>; NUM_ROUNDS] = std::array::from_fn(|_| U32::constant(console::U32::new(0)));
             for t in 0..16 {
                 let slice = &chunk[t * WORD_SIZE..(t + 1) * WORD_SIZE];
                 w[t] = U32::from_bits_be(slice);
             }
             for t in 16..NUM_ROUNDS {
-                let s0 = rotate_right(&w[t - 15], 7)
-                    ^ rotate_right(&w[t - 15], 18)
-                    ^ shift_right(&w[t - 15], 3);
-                let s1 = rotate_right(&w[t - 2], 17)
-                    ^ rotate_right(&w[t - 2], 19)
-                    ^ shift_right(&w[t - 2], 10);
-                w[t] = &(&(&w[t - 16] + &s0) + &w[t - 7]) + &s1;
+                // sigma_0(W_t-15):
+                let s0 = rotate_right(&w[t - 15], 7) ^ rotate_right(&w[t - 15], 18) ^ shift_right(&w[t - 15], 3);
+                // sigma_1(W_t-2):
+                let s1 = rotate_right(&w[t - 2], 17) ^ rotate_right(&w[t - 2], 19) ^ shift_right(&w[t - 2], 10);
+                w[t] = s1.add_wrapped(&w[t - 7]).add_wrapped(&s0).add_wrapped(&w[t - 16]);
             }
 
+            // Step 2 in FIPS PUB 180-4, Section 6.2.2.
             let mut a = state[0].clone();
             let mut b = state[1].clone();
             let mut c = state[2].clone();
@@ -54,63 +73,88 @@ impl<E: Environment, const VARIANT: usize> Hash for Sha2<E, VARIANT> {
             let mut g = state[6].clone();
             let mut h = state[7].clone();
 
+            // Step 3 in FIPS PUB 180-4, Section 6.2.2.
             for t in 0..NUM_ROUNDS {
-                let S1 = rotate_right(&e, 6)
-                    ^ rotate_right(&e, 11)
-                    ^ rotate_right(&e, 25);
+                // Sigma_1(e):
+                let S1 = rotate_right(&e, 6) ^ rotate_right(&e, 11) ^ rotate_right(&e, 25);
+                // Ch(e, f, g):
                 let ch = (&e & &f) ^ ((!&e) & &g);
-                let temp1 = &(&(&(&h + &S1) + &ch) + &self.constants[t]) + &w[t];
-                let S0 = rotate_right(&a, 2)
-                    ^ rotate_right(&a, 13)
-                    ^ rotate_right(&a, 22);
+                // T1:
+                let temp1 = h.add_wrapped(&S1).add_wrapped(&ch).add_wrapped(&constants[t]).add_wrapped(&w[t]);
+                // Sigma_0(a):
+                let S0 = rotate_right(&a, 2) ^ rotate_right(&a, 13) ^ rotate_right(&a, 22);
+                // Maj(a, b, c):
                 let maj = (&a & &b) ^ (&a & &c) ^ (&b & &c);
-                let temp2 = &S0 + &maj;
+                // T2:
+                let temp2 = S0.add_wrapped(&maj);
 
                 h = g;
                 g = f;
                 f = e;
-                e = &d + &temp1;
+                e = d.add_wrapped(&temp1);
                 d = c;
                 c = b;
                 b = a;
-                a = &temp1 + &temp2;
+                a = temp1.add_wrapped(&temp2);
             }
 
-            for i in 0..8 {
-                state[i] = &state[i] + &[a.clone(), b.clone(), c.clone(), d.clone(), e.clone(), f.clone(), g.clone(), h.clone()][i];
-            }
+            // Step 4 in FIPS PUB 180-4 Section 6.2.2.
+            state[0] = a.add_wrapped(&state[0]);
+            state[1] = b.add_wrapped(&state[1]);
+            state[2] = c.add_wrapped(&state[2]);
+            state[3] = d.add_wrapped(&state[3]);
+            state[4] = e.add_wrapped(&state[4]);
+            state[5] = f.add_wrapped(&state[5]);
+            state[6] = g.add_wrapped(&state[6]);
+            state[7] = h.add_wrapped(&state[7]);
         }
 
-        let mut output = Vec::with_capacity(VARIANT);
+        // Turn the final has value into a sequence of bits.
+        let mut output = Vec::with_capacity(256);
         for word in state.iter() {
             output.extend(word.to_bits_be());
         }
+
+        // Keep and return 256 or 224 bits.
         output.truncate(VARIANT);
         output
     }
 }
 
+/// Pad the input bits to a multiple of the block size.
+/// See FIPS PUB 180-4, Section 5.1.1.
 fn pad_sha2<E: Environment>(input: &[Boolean<E>]) -> Vec<Boolean<E>> {
     let mut padded = input.to_vec();
+
+    // Add a '1' bit.
     padded.push(Boolean::constant(true));
-    while (padded.len() + 64) % CHUNK_SIZE != 0 {
+
+    // Add as many '0' bits as needed to make the length congruent to 448 mod 512,
+    // so that there are exactly 64 bits left for the length (448 + 64 = 512).
+    while (padded.len() + 64) % BLOCK_SIZE != 0 {
         padded.push(Boolean::constant(false));
     }
-    let len_bytes = (input.len() as u64).to_be_bytes();
-    for byte in len_bytes.iter() {
-        for i in (0..8).rev() {
-            padded.push(Boolean::constant((byte >> i) & 1 == 1));
-        }
+
+    // This cast never fails because we checked the input length in `hash`.
+    let len = input.len() as u64;
+    // We allocate new circuit variables for the 64 bits of the length because they depend on the input.
+    // TODO: perhaps we can allocate constants instead, given that the circuit is for the particular input length
+    for i in (0..64).rev() {
+        padded.push(Boolean::new(Mode::Private, (len >> i) & 1 == 1));
     }
+
+    // The result has a length that is a multiple of the block size.
     padded
 }
 
+/// The `ROTR^n` operation in FIPS PUB 180-4, Section 3.2.
 fn rotate_right<E: Environment>(value: &U32<E>, n: usize) -> U32<E> {
     let mut bits = value.to_bits_be();
     bits.rotate_right(n);
     U32::from_bits_be(&bits)
 }
 
+/// The `SHR^n` operation in FIPS PUB 180-4, Section 3.2.
 fn shift_right<E: Environment>(value: &U32<E>, n: usize) -> U32<E> {
     let mut bits = value.to_bits_be();
     for _ in 0..n {
@@ -118,4 +162,50 @@ fn shift_right<E: Environment>(value: &U32<E>, n: usize) -> U32<E> {
         bits.pop();
     }
     U32::from_bits_be(&bits)
+}
+
+#[cfg(test)]
+mod tests {
+    use snarkvm_circuit_types::{
+        Boolean,
+        environment::{Circuit, Eject, Inject},
+    };
+    use crate::{Hash, Sha2_256};
+
+    fn bits_to_hex_string(bits: &[Boolean<Circuit>]) -> String {
+        let mut string = String::new();
+        for four_bits in bits.chunks(4) {
+            let hex_digit = four_bits.iter().fold(0, |acc, bit| (acc << 1) + bit.eject_value() as u8);
+            string.push_str(&format!("{:01x}", hex_digit));
+        }
+        string
+    }
+
+    fn string_to_bits(string: &str) -> Vec<Boolean<Circuit>> {
+        let mut bits = Vec::new();
+        for c in string.chars() {
+            let byte = c as u8;
+            for i in (0..8).rev() {
+                bits.push(Boolean::constant((byte >> i) & 1 == 1));
+            }
+        }
+        bits
+    }
+
+    fn test_input_output(input: &str, output: &str) {
+        let sha = Sha2_256::<Circuit>::new();
+        let input_bits = string_to_bits(input);
+        let output_bits = sha.hash(&input_bits);
+        assert_eq!(bits_to_hex_string(&output_bits), output);
+    }
+
+    #[test]
+    fn test() {
+        test_input_output("", "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+        test_input_output("abc", "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad");
+        test_input_output(
+            "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
+            "248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1",
+        );
+    }
 }
--- circuit/algorithms/src/sha2/mod.rs
+++ circuit/algorithms/src/sha2/mod.rs
@@ -13,8 +13,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! This module implements SHA-224 and SHA-256.
+//! It is based on NIST FIPS PUB 180-4, Secure Hash Standard (SHS).
 mod hash;
 
+use std::marker::PhantomData;
 #[cfg(all(test, feature = "console"))]
 use snarkvm_circuit_types::environment::assert_scope;
 #[cfg(test)]
@@ -28,60 +31,47 @@ pub type Sha2_224<E> = Sha2<E, 224>;
 /// The SHA-2 256 hash function.
 pub type Sha2_256<E> = Sha2<E, 256>;
 
-const CHUNK_SIZE: usize = 512;
+/// This is `m` in FIPS PUB 180-4 (Section 2.2.1).
+const BLOCK_SIZE: usize = 512;
+
+/// This is `w` in FIPS PUB 180-4 (Section 2.2.1).
 const WORD_SIZE: usize = 32;
+
+/// Number of elements in the message schedule `{W_t}` in FIPS PUB 180-4 (Section 6.2.2, step 1).
 const NUM_ROUNDS: usize = 64;
 
 /// The SHA-2 hash function.
 #[derive(Clone, Debug)]
 pub struct Sha2<E: Environment, const VARIANT: usize> {
-    /// Round constants.
-    constants: [U32<E>; NUM_ROUNDS],
-    /// Initial hash state.
-    initial_state: [U32<E>; 8],
+    phantom_data: PhantomData<E>
 }
 
 impl<E: Environment, const VARIANT: usize> Sha2<E, VARIANT> {
     /// Initializes a new SHA-2 hash function.
     pub fn new() -> Self {
-        // Initialize round constants using std::array::from_fn.
-        let constants = std::array::from_fn(|i| U32::constant(console::U32::new(K[i])));
-        // Initialize initial state depending on variant (224 or 256).
-        let initial_state = match VARIANT {
-            256 => std::array::from_fn(|i| U32::constant(console::U32::new(H0_256[i]))),
-            224 => std::array::from_fn(|i| U32::constant(console::U32::new(H0_224[i]))),
-            _ => unreachable!("Invalid SHA-2 variant"),
-        };
-        Self { constants, initial_state }
+        Self { phantom_data : PhantomData}
     }
 }
 
-// First 32 bits of the fractional parts of the cube roots of the first 64 primes.
+/// First 32 bits of the fractional parts of the cube roots of the first 64 primes.
+/// See FIPS PUB 180-4, Section 4.2.2.
 const K: [u32; NUM_ROUNDS] = [
-    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
-    0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
-    0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
-    0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
-    0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
-    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
-    0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
-    0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
-    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98,
+    0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786,
+    0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 0x983e5152, 0xa831c66d, 0xb00327c8,
+    0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819,
+    0xd6990624, 0xf40e3585, 0x106aa070, 0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a,
+    0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7,
+    0xc67178f2,
 ];
 
-const H0_256: [u32; 8] = [
-    0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
-    0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
-];
+/// Initial hash value for SHA-256.
+/// See FIPS PUB 180-4, Section 5.3.3.
+const H0_256: [u32; 8] =
+    [0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19];
 
-const H0_224: [u32; 8] = [
-    0xc1059ed8, 0x367cd507, 0x3070dd17, 0xf70e5939,
-    0xffc00b31, 0x68581511, 0x64f98fa7, 0xbefa4fa4,
-];
+/// Initial hash value for SHA-224.
+/// See FIPS PUB 180-4, Section 5.3.2.
+const H0_224: [u32; 8] =
+    [0xc1059ed8, 0x367cd507, 0x3070dd17, 0xf70e5939, 0xffc00b31, 0x68581511, 0x64f98fa7, 0xbefa4fa4];
